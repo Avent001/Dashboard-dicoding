4664b60663858e97346a413d3151972b9c77129f
Avent001 <104295337+Avent001@users.noreply.github.com>
2024-03-03 22:52:32 +0700

    Update ProjectAkhir.py

--- Dashboard/ProjectAkhir.py
+++ Dashboard/ProjectAkhir.py
@@ -110,7 +110,7 @@ def Holiday_analysis(df):
     print(f'Mean Weekday: {mean_non_holiday}, Std Weekday: {std_non_holiday}')
 
     return fig, ax
-data = pd.read_csv("D:\Kuliahan\Bangkit\ProjectAkhir\Bike-sharing-dataset\hour.csv")
+data = pd.read_csv("/workspaces/Dashboard-dicoding/Bike-sharing-dataset/hour.csv")
 st.sidebar.title("Bike Sharing Analysis Dashboard")
 
 
